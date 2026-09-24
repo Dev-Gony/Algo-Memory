230eab8a073556eb64b5d621fa281f47c660dd31
Dev-Gony <gwonyi190000@gmail.com>
2026-09-24 18:21:53 +0900

    chore: add public Neon endpoints

---
+++ src/neon-config.js
@@ -0,0 +1,4 @@
+window.ALGO_MEMORY_NEON = {
+  authUrl: 'https://ep-red-king-b53etqf9.neonauth.c-7.us-east-2.aws.neon.tech/neondb/auth',
+  dataApiUrl: 'https://ep-red-king-b53etqf9.apirest.c-7.us-east-2.aws.neon.tech/neondb/rest/v1'
+};
